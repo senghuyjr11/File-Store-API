6948f617ee9ffb0a6578d2e10924162e435b1ed1
senghuyjr11 <senghuymit007@gmail.com>
2022-11-01 22:43:19 +0700

    first commit

---
+++ src/test/java/com/example/filestoreapi/FileStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filestoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
